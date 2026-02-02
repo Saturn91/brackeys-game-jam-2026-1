09fcd5b7592bc2ea7c44cba849beedde6a447e22
Manuel Geissberger <manuel.geissberger@gmail.com>
2026-02-02 18:59:52 +0100

    debug properties menu

--- src/debug/debug-properties/debug_properties.gd
+++ src/debug/debug-properties/debug_properties.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _ready() -> void:
 	if !OS.has_feature("editor"): hide()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("toggle_debug"):
+		if visible: hide()
+		else: show()
